c39f689c20e5c3480e2bc70fa38c09a6f08affca
matsuokanao <matsuokanao1029@gmail.com>
2020-08-21 18:47:31 +0900

    HOME画面実装

--- app/src/main/java/com/example/messengerapp/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/messengerapp/Fragments/SearchFragment.kt
@@ -1,10 +1,15 @@
 package com.example.messengerapp.Fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.messengerapp.AdapterClasses.UserAdapter
 import com.example.messengerapp.ModelClasses.Users
 import com.example.messengerapp.R
@@ -13,6 +18,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_search.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +30,9 @@ class SearchFragment : Fragment() {
 
     private var userAdapter: UserAdapter? = null
     private var mUsers: List<Users>? = null
+    private var recyclerView: RecyclerView? = null
+    private var searchEditText: EditText? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +41,28 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_search, container, false)
 
+        recyclerView = view.findViewById(R.id.searchList)
+        recyclerView!!.setHasFixedSize(true)
+        recyclerView!!.layoutManager = LinearLayoutManager(context)
+        searchEditText = view.findViewById(R.id.searchUsersET)
+
         mUsers = ArrayList()
         retrieveAllUsers()
+
+        searchEditText!!.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(cs: CharSequence?, start: Int, befor: Int, count: Int) {
+                searchForUsers(cs.toString().toLowerCase())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
         return view
     }
 
@@ -47,18 +76,19 @@ class SearchFragment : Fragment() {
             override fun onDataChange(p0: DataSnapshot) {
 
                 (mUsers as ArrayList<Users>).clear()
-                for (snapshot in p0.children){
-                   val user: Users? = p0.getValue(Users::class.java)
+                if (searchEditText!!.text.toString() == "") {
+                    for (snapshot in p0.children) {
+                        val user: Users? = snapshot.getValue(Users::class.java)
 
-                    if(!(user!!.getUID()).equals(firebaseUserID)){
-                        (mUsers as ArrayList<Users>).add(user)
+                        if (!(user!!.getUID()).equals(firebaseUserID)) {
+                            (mUsers as ArrayList<Users>).add(user)
+                        }
                     }
+                    userAdapter = UserAdapter(context!!, mUsers!!, false)
+                    recyclerView!!.adapter = userAdapter
                 }
-                userAdapter = UserAdapter(context!!,mUsers!!,false)
             }
-
-            override fun onCancelled(error: DatabaseError) {
-
+                override fun onCancelled(error: DatabaseError) {
             }
         })
     }
@@ -77,13 +107,14 @@ class SearchFragment : Fragment() {
                 (mUsers as ArrayList<Users>).clear()
 
                 for (snapshot in p0.children){
-                    val user: Users? = p0.getValue(Users::class.java)
+                    val user: Users? = snapshot.getValue(Users::class.java)
 
                     if(!(user!!.getUID()).equals(firebaseUserID)){
                         (mUsers as ArrayList<Users>).add(user)
                     }
                 }
                 userAdapter = UserAdapter(context!!,mUsers!!,false)
+
             }
 
             override fun onCancelled(error: DatabaseError) {
--- app/src/main/java/com/example/messengerapp/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/messengerapp/Fragments/SettingsFragment.kt
@@ -1,60 +1,173 @@
 package com.example.messengerapp.Fragments
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.messengerapp.ModelClasses.Users
 import com.example.messengerapp.R
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Request
+import kotlinx.android.synthetic.main.fragment_settings.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var usersRefrence: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+    private val RequesteCode = 483
+    private var imageeUri: Uri? = null
+    private var storgeRef: StorageReference? = null
+    private var coverChecker: String? = ""
+    private var socialChecker: String? = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        usersRefrence = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        storgeRef = FirebaseStorage.getInstance().reference.child("User Images")
+
+        usersRefrence!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()){
+
+                    val user: Users? = p0.getValue(Users::class.java)
+
+                    if (context!=null){
+
+                        view.username_settings.text = user!!.getUserName()
+                        Picasso.get().load(user.getProfile()).into(view.profile_image_settings)
+                        Picasso.get().load(user.getCover()).into(view.cover_image_setting)
+
+                    }
                 }
             }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+        })
+        view.profile_image_settings.setOnClickListener {
+
+            PickImage()
+
+        }
+        view.cover_image_setting.setOnClickListener {
+            coverChecker = "cover"
+
+            PickImage()
+
+        }
+
+        view.set_facebook.setOnClickListener {
+            socialChecker = "facebook"
+            setSocialLinks()
+
+        }
+
+        view.set_instagram.setOnClickListener {
+            socialChecker = "instagram"
+            setSocialLinks()
+
+        }
+
+        view.set_website.setOnClickListener {
+            socialChecker = "website"
+            setSocialLinks()
+
+        }
+        return view
     }
+
+    private fun setSocialLinks() {
+        val builder: AlertDialog.Builder =
+            AlertDialog.Builder(context!!,R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+    }
+
+    private fun PickImage() {
+        val intent = Intent()
+        intent.type = "image/"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent,RequesteCode)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RequesteCode && resultCode == Activity.RESULT_OK && data!!.data != null){
+
+            imageeUri = data.data
+            Toast.makeText(context, "uploading...",Toast.LENGTH_LONG).show()
+            uploadImageToDatabase()
+
+        }
+    }
+
+    private fun uploadImageToDatabase() {
+        val progressBar = ProgressDialog(context)
+        progressBar.setMessage("画像アップロード中です。しばらくお待ちください。")
+        progressBar.show()
+
+        if (imageeUri!=null){
+
+            val fileRef = storgeRef!!.child(System.currentTimeMillis().toString()+ ".jpg")
+            var uploadTask: StorageTask<*>
+            uploadTask = fileRef.putFile(imageeUri!!)
+
+            uploadTask.continueWithTask(Continuation <UploadTask.TaskSnapshot, Task<Uri>>{ task ->
+                if (!task.isSuccessful){
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation fileRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    val downloadUrl = task.result
+                    val url = downloadUrl.toString()
+
+                    if (coverChecker == "cover"){
+
+                        val  mapCoverImg = HashMap<String,Any>()
+                        mapCoverImg["cover"] = url
+                        usersRefrence!!.updateChildren(mapCoverImg)
+                        coverChecker = ""
+
+                    } else {
+
+                        val  mapProfileImg = HashMap<String,Any>()
+                        mapProfileImg["profile"] = url
+                        usersRefrence!!.updateChildren(mapProfileImg)
+                        coverChecker = ""
+
+                    }
+                    progressBar.dismiss()
+                }
+            }
+        }
+
+    }
+
 }
